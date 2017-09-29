88ab9b03144a1f4157e1542133744084745911a0
tupamba <tupamba@gmail.com>
2017-09-29 12:04:49 -0300

    update

--- src/app/patient/add/patient-add.component.ts
+++ src/app/patient/add/patient-add.component.ts
@@ -13,43 +13,61 @@ import { MyDatePickerModule } from 'mydatepicker/src/my-date-picker/my-date-pick
 export class PatientAddComponent implements OnInit {
   listPatients: Patient[];
   currentPatient: Patient;
-  model:modelView;
+  model: modelView;
   myDatePickerOptions: any;
-  constructor(private _route: ActivatedRoute, private _service: PatientService,private _media: MediaService) { }
+  urlImg: string;
+  file: File;
+  constructor(private _route: ActivatedRoute, private _service: PatientService, private _media: MediaService) { }
 
   ngOnInit() {
-    this.model = new modelView("","","");
+    this.model = new modelView("", "", "");
     this.listPatients = this._service.getCurrentPatients();
     this._route.params.forEach((params: Params) => {
       let id = params['id'];
       this.currentPatient = this.listPatients.find(x => x._id == id);
     });
   }
-  onSubmit()
-  {
+  onSubmit() {
     console.log("submit");
   }
-  onDateChanged(event)
-  {
+  onDateChanged(event) {
     this.model.birthDate = event;
   }
   fileChange(event) {
+    console.log("fileChange");
+    let reader = new FileReader();
     let fileList: FileList = event.target.files;
-   
-    if(fileList.length > 0) {
-        let file: File = fileList[0];
-        this._media.folder = "images";
-        this._media.upload(file);
+    if (fileList.length > 0) {
+      this.file = fileList[0];
+      this.readFile(this.file, reader, (result) => {
+      });
+      reader.readAsDataURL(this.file);
     }
-}
+
+  }
+  uploadFile() {
+    console.log("uploadFile " + this.file.name);
+    this._media.folder = "images";
+    this._media.upload(this.file);
+    this.urlImg = "";
+  }
+  readFile(file: File, reader, callback) {
+
+    reader.onload = () => {
+      callback(reader.result);
+      this.urlImg = reader.result;
+      console.log(reader.result);
+
+    }
+  }
 
 }
 export class modelView {
   constructor(
-      public name: string,
-      public document:string,
-      public birthDate: string,
+    public name: string,
+    public document: string,
+    public birthDate: string,
   ) {
-      
+
   }
 }
